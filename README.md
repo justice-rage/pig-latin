9fe2a221e30dd71be695942a7f1437f9a56b70a1
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-06 15:02:12 -0800

    add logic to handle capital vowels and new second test
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 Describe: pigLatin()
 
-Test 1: "It will add 'way' to the end of words that begin with a vowel."
+Test 1: "It will add 'way' to the end of words that begin with a lowercase vowel."
 Code: pigLatin("a");
 Expected Output: "away"
 
-Test 2: "It will add 'ay' to the end of words that begin with one or more consonants."
+Test 2: "It will add 'way' to the end of words that begin with an uppercase vowel."
+Code: pigLatin("A");
+Expected Output: "Away"
+
+Test 3: "It will add 'ay' to the end of words that begin with one or more consonants."
 Code: pigLatin("code");
 Expected Output: "odecay"
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
+// For words beginning with a vowel, add "way" to the end. For Pig Latin, vowels are "a," "e," "i," "o," and "u." Don't treat "y" as a vowel. Examples: "away" becomes "awayway" and "okay" becomes "okayway."
+
+// For words beginning with one or more consonants, move all of the first consecutive consonants to the end and add "ay". Examples: "code" becomes "odecay" and "move" becomes "ovemay."
+
+
+
 function pigLatin(element) {
-  const sequence = ["a", "e", "i", "o", "u"];
+  let firstConsonants = [];
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   let wordArray = element.split("");
 
-    if (sequence.includes(wordArray[0])) {
+    if (vowels.includes(wordArray[0])) {
       return wordArray.join("") + "way"; 
     }
     return i;
