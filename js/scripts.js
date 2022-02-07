49e3d49cef88649ecec279b396f8a6ab5be781d7
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-06 16:32:56 -0800

    end of day work complete
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 
 // For words beginning with one or more consonants, move all of the first consecutive consonants to the end and add "ay". Examples: "code" becomes "odecay" and "move" becomes "ovemay."
 
-
+// If the first consonants include "qu", move the "q" and the "u." Don't forget about words like "squeal" where "qu" doesn't come first! Examples: "quick" becomes "ickquay" while "squeal" becomes "quealsay."
 
 function pigLatin(element) {
   let firstConsonants = [];
-  const vowels = ["a", "e", "i", "o", "u"];
+  const vowels = ["a", "e", "i", "o", "u",];
   let wordArray = element.toLowerCase().split("");
 
     if (vowels.includes(wordArray[0])) {
